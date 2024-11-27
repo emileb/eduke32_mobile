ea105f4edbb99835c5bc68f093fb69fe5e8a7199
Emile Belanger <emile.belanger@gmail.com>
2024-11-27 13:48:12 +0000

    Fixed LogWritter

--- source/build/src/osd.cpp
+++ source/build/src/osd.cpp
@@ -15,6 +15,11 @@
 #endif
 #include "atomiclist.h"
 
+#ifdef __ANDROID__
+#include <android/log.h>
+#include "LogWritter.h"
+#endif
+
 #define XXH_STATIC_LINKING_ONLY
 #include "xxhash.h"
 
@@ -1723,7 +1728,10 @@ void OSD_Puts(const char *putstr)
 {
     if (putstr[0] == 0 || !osd)
         return;
-
+#ifdef __ANDROID__
+    __android_log_print(ANDROID_LOG_INFO,"DUKE", "%s",putstr);
+     LogWritter_Write(putstr);
+#endif
     osd->log.m_pending.push(new AtomicLogString(Xstrdup(putstr)));
     OSD_WritePendingLines();
 }
