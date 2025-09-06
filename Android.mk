323b80e90717fa1d2caaad0b7ef3a9692a396c82
Emile Belanger <emile.belanger@gmail.com>
2025-09-06 18:32:10 +0100

    Fix for Mac build

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@ $(LOCAL_PATH)/libxmp-lite/include \
 $(SDL_INCLUDE_PATHS) \
 $(TOP_DIR)/Clibs_OpenTouch \
 $(TOP_DIR)/Clibs_OpenTouch/libvpx/include \
-$(TOP_DIR)/Clibs_OpenTouch\raze \
+$(TOP_DIR)/Clibs_OpenTouch/raze \
 $(TOP_DIR)/MobileTouchControls  \
 $(TOP_DIR)/AudioLibs_OpenTouch/liboggvorbis/include  \
 
